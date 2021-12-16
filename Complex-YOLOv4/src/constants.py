d2b9ba0ecf8c5c3d673b5d272f72a1fa8f6b4e7f
YuanzhiZhu <yuazhu@student.ethz.ch>
2021-12-16 18:13:07 +0100

    fix some problem
    fix some problem

--- Complex-YOLOv4/src/constants.py
+++ Complex-YOLOv4/src/constants.py
@@ -1,9 +1,10 @@
-base_dir_detections_fd = './data/your_dataset/base_dir_detections_fd' # Directory that contains detections by fine level detector
-base_dir_detections_cd = './data/your_dataset/base_dir_detections_cd' # Directory that contains detections by coarse level detector
-base_dir_groundtruth = './data/your_dataset/base_dir_groundtruth' # Directory that contains ground truth bounding boxes
-base_dir_metric_fd = './data/your_dataset/base_dir_metric_fd' # Directory that contains AP or AR values by the fine detector
-base_dir_metric_cd = './data/your_dataset/base_dir_metric_cd' # Directory that contains AP or AR values by the coarse detector
+base_dir_detections_fd = '../dataset/kitti/base_dir_detections_fd' # Directory that contains detections by fine level detector
+base_dir_detections_cd = '../dataset/kitti/base_dir_detections_cd' # Directory that contains detections by coarse level detector
+base_dir_groundtruth = '../dataset/kitti/base_dir_groundtruth' # Directory that contains ground truth bounding boxes
+base_dir_metric_fd = '../dataset/kitti/base_dir_metric_fd' # Directory that contains AP or AR values by the fine detector
+base_dir_metric_cd = '../dataset/kitti/base_dir_metric_cd' # Directory that contains AP or AR values by the coarse detector
+
 num_actions = 16 # Hyperparameter, should be equal to num_windows * num_windows
 num_windows = 4 # Number of windows in one dimension
-img_size_fd = 256 # Image size used to train the fine level detector
-img_size_cd = 64 # Image size used to train the coarse level detector
+img_size_fd = 880 # Image size used to train the fine level detector
+img_size_cd = 220 # Image size used to train the coarse level detector
