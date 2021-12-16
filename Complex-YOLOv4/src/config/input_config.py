9f443e00f1d12a760d9fd2edda7a2f779b8bf04b
ZetongZhangZ <71453083+ZetongZhangZ@users.noreply.github.com>
2021-12-16 15:39:47 +0100

    update based on current parameters

--- Complex-YOLOv4/src/config/input_config.py
+++ Complex-YOLOv4/src/config/input_config.py
@@ -1,42 +1,51 @@
 from easydict import EasyDict as edict
 
+# unit: m
+RANGE = 60
 
 def complete_info(d):
-    OVERLAP = (float(d.PATCH_NUM) * d.PATCH_SIZE - d.IMAGE_SIZE) / (d.PATCH_NUM - 1.) if d.PATCH_NUM > 1 else 0
+    OVERLAP = (float(d.PATCH_NUM) * d.PATCH_SIZE - img_size) / (d.PATCH_NUM - 1.) if d.PATCH_NUM > 1 else 0
     # Front side (of vehicle) Point Cloud boundary for BEV
-    d.boundary = [{"minX": 50. / d.IMAGE_SIZE * (d.PATCH_SIZE * i - OVERLAP * i),
-                   "maxX": 50. / d.IMAGE_SIZE * (d.PATCH_SIZE * (i + 1) - OVERLAP * i),
-                   "minY": -25. + 50. / d.IMAGE_SIZE * (d.PATCH_SIZE * j - OVERLAP * j),
-                   "maxY": -25. + 50. / d.IMAGE_SIZE * (d.PATCH_SIZE * (j + 1) - OVERLAP * j),
+    d.boundary = [{"minX": RANGE * (d.PATCH_SIZE * i - OVERLAP * i) / img_size,
+                   "maxX": RANGE * (d.PATCH_SIZE * (i + 1) - OVERLAP * i) /img_size ,
+                   "minY": -RANGE/2 + RANGE * (d.PATCH_SIZE * j - OVERLAP * j) / img_size ,
+                   "maxY": -RANGE/2 + RANGE * (d.PATCH_SIZE * (j + 1) - OVERLAP * j) / img_size ,
                    "minZ": -2.73,
                    "maxZ": 1.27} for i in range(d.PATCH_NUM) for j in range(d.PATCH_NUM)]
     d.DISCRETIZATION = [(d.boundary[i]["maxX"] - d.boundary[i]["minX"]) / d.BEV_HEIGHT for i in range(len(d.boundary))]
     return
 
-
-img_size = 608.
+# unit: pixel
+img_size = 880.
+patch_size = 256.
+downsample = 4
+patch_num = 4  # (1d)
 
 LR_IMAGE_CFG = edict({
-    "PATCH_NUM": 1, "IMAGE_SIZE": img_size, "PATCH_SIZE": img_size,
-    "BEV_WIDTH": int(img_size/5), "BEV_HEIGHT": int(img_size/5)
+    "PATCH_NUM": 1,
+    "PATCH_SIZE": img_size,
+    "BEV_WIDTH": int(img_size / downsample), "BEV_HEIGHT": int(img_size / downsample)
 })
 complete_info(LR_IMAGE_CFG)
 
 HR_IMAGE_CFG = edict({
-    "PATCH_NUM": 1, "IMAGE_SIZE": img_size, "PATCH_SIZE": img_size,
+    "PATCH_NUM": 1,
+    "PATCH_SIZE": img_size,
     "BEV_WIDTH": int(img_size), "BEV_HEIGHT": int(img_size)
 })
 complete_info(HR_IMAGE_CFG)
 
 LR_PATCH_CFG = edict({
-    "PATCH_NUM": 2, "IMAGE_SIZE": img_size, "PATCH_SIZE": 320.,
-    "BEV_WIDTH": 64, "BEV_HEIGHT": 64
+    "PATCH_NUM": patch_num,
+    "PATCH_SIZE": patch_size,
+    "BEV_WIDTH": int(patch_size / downsample), "BEV_HEIGHT": int(patch_size / downsample)
 })
 complete_info(LR_PATCH_CFG)
 
 HR_PATCH_CFG = edict({
-    "PATCH_NUM": 2, "IMAGE_SIZE": img_size, "PATCH_SIZE": 320.,
-    "BEV_WIDTH": 320, "BEV_HEIGHT": 320
+    "PATCH_NUM": patch_num,
+    "PATCH_SIZE": patch_size,
+    "BEV_WIDTH": int(patch_size), "BEV_HEIGHT": int(patch_size)
 })
 complete_info(HR_PATCH_CFG)
 
@@ -49,3 +58,9 @@ complete_info(HR_PATCH_CFG)
 #     "minZ": -2.73,
 #     "maxZ": 1.27
 # }
+
+if __name__ == "__main__":
+    print(LR_IMAGE_CFG)
+    print(LR_PATCH_CFG)
+    print(HR_IMAGE_CFG)
+    print(HR_PATCH_CFG)
