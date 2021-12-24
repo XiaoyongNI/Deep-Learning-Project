8951bd4ab0977396cc8d8b91a5211b340f685a29
XiaoyongNI <KalmanNet.ai@gmail.com>
2021-12-24 11:24:54 +0100

    update RL training data

--- Complex-YOLOv4/src/constants.py
+++ Complex-YOLOv4/src/constants.py
@@ -1,8 +1,8 @@
 base_dir_detections_fd = '../dataset/kitti/base_dir_detections_fd' # Directory that contains detections by fine level detector
 base_dir_detections_cd = '../dataset/kitti/base_dir_detections_cd' # Directory that contains detections by coarse level detector
 base_dir_groundtruth = '../dataset/kitti/base_dir_groundtruth' # Directory that contains ground truth bounding boxes
-base_dir_metric_fd = '../dataset/kitti/base_dir_metric_fd' # Directory that contains AP or AR values by the fine detector
-base_dir_metric_cd = '../dataset/kitti/base_dir_metric_cd' # Directory that contains AP or AR values by the coarse detector
+base_dir_metric_fd = '../dataset/kitti/' # Directory that contains AP or AR values by the fine detector
+base_dir_metric_cd = '../dataset/kitti/' # Directory that contains AP or AR values by the coarse detector
 
 num_actions = 16 # Hyperparameter, should be equal to num_windows * num_windows
 num_windows = 4 # Number of windows in one dimension
